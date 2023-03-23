315b3ddfd936679309755a24e1e004d057d23a6c
Ravi Batistel <ravi.batistel@outlook.com>
2023-03-23 08:48:31 -0300

    Push to git

---
+++ src/main/java/com/studiosol/passwordverifier/PasswordverifierApplication.java
@@ -0,0 +1,13 @@
+package com.studiosol.passwordverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordverifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordverifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiosol/passwordverifier/PasswordverifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiosol.passwordverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordverifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
